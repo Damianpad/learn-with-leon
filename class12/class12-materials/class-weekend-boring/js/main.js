4378d7dea2500f03fa8b20ceb560511b2a45467b
Damian Padilla <damipad4@gmail.com>
2022-10-13 11:16:00 -0500

    Completed class-weekend-boring

--- class12/class12-materials/class-weekend-boring/js/main.js
+++ class12/class12-materials/class-weekend-boring/js/main.js
@@ -4,7 +4,18 @@ function check() {
 
   const day = document.querySelector('#day').value
 
-  //Conditionals go here
+  let result = day.toLowerCase();
+
+  if (result === 'tuesday' || result === 'thursday') {
+    message = 'Today is a class day'
+    document.querySelector('#placeToSee').innerText = message;
+  } else if (result === 'saturday' || result === 'sunday'){
+    message = 'It\'s the weekend!'
+    document.querySelector('#placeToSee').innerText = message;
+  } else {
+    message = 'BOOOORINNNNGGGG'
+    document.querySelector('#placeToSee').innerText = message;
+  }
 
 
 }

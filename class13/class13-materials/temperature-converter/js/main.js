eaf879a12d50b3e254b1e95c3c49508847c31ad8
Damian Padilla <damipad4@gmail.com>
2022-10-15 23:58:04 -0500

    practice pseudo code for temp

--- class13/class13-materials/temperature-converter/js/main.js
+++ class13/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,15 @@
 //Write your pseduo code first! 
+
+/*
+Value from user is entered into an input box. Example: 50
+
+
+
+When the user presses a 'convert' button:
+    The value that is in the input box is stored as a variable called fahrenheit
+    The fahrenheit (50) variable is converted into C using this equation:
+    Subtract 30 from fahrenheit and then divide by 2.
+
+Converted value is displayed in the DOM by using an h2 with the class of .placeToSee
+
+*/

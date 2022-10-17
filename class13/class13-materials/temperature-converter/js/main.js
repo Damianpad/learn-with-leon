dd8a480b6994893715d08c68bf5c4e04717950ff
Damian Padilla <damipad4@gmail.com>
2022-10-16 20:03:07 -0500

    Coded my solution for temp converter

--- class13/class13-materials/temperature-converter/js/main.js
+++ class13/class13-materials/temperature-converter/js/main.js
@@ -5,6 +5,7 @@ Value from user is entered into an input box. Example: 50
 
 
 
+
 When the user presses a 'convert' button:
     The value that is in the input box is stored as a variable called fahrenheit
     The fahrenheit (50) variable is converted into C using this equation:
@@ -13,3 +14,15 @@ When the user presses a 'convert' button:
 Converted value is displayed in the DOM by using an h2 with the class of .placeToSee
 
 */
+
+document.querySelector('#convert').addEventListener('click', run);
+
+function run() {
+    const fahrenheit = document.querySelector('#fahrenheit').value
+    const celsius = (fahrenheit - 32) * 5/9
+    document.querySelector('#placeToSee').innerText = `${fahrenheit} Fahrenheit is converted to ${celsius} celsius`
+}
+
+
+
+

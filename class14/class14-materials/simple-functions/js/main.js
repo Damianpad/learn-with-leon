ef54da4dc1984778716f3cc804910c6b6156639c
Damian Padilla <damipad4@gmail.com>
2022-10-17 21:08:33 -0500

    Completed func practice

--- class14/class14-materials/simple-functions/js/main.js
+++ class14/class14-materials/simple-functions/js/main.js
@@ -1,5 +1,17 @@
 //create a function that adds two numbers and alerts the sum
+function addition(num1, num2) {
+    alert(num1 + num2)
+}
 
+addition(4,5)
 //create a function that multiplys three numbers and console logs the product
+function multiplication(num1, num2, num3) {
+    console.log(num1 * num2 * num3);
+}
 
+multiplication(2,2,2)
 //create a function that divides two numbers and returns the ???
+function division(num1, num2) {
+    return console.log(num1 / num2);
+}
+division(10,2);

f7bfa56679e00b60d099d4c061103decc44cf494
Damian Padilla <damipad4@gmail.com>
2022-10-19 11:30:18 -0500

    Completed B practice

--- class13/class13-materials/bachelor-code/1/js/main.js
+++ class13/class13-materials/bachelor-code/1/js/main.js
@@ -1,6 +1,7 @@
-document.querySelector('#finalRose').addEventListener('click', hide)
+//Write a program that hides claire and sharleen when the Final Rose is clicked
+document.querySelector('#finalRose').addEventListener('click', rose)
 
-function hide(){
-	document.querySelector('#claire').style.display = 'none'
-	document.querySelector('#sharleen').style.display = 'none'
+function rose(){
+	document.querySelector('#claire').style.display = 'none';
+	document.querySelector('#sharleen').style.display = 'none';
 }
--- class13/class13-materials/bachelor-code/2/js/main.js
+++ class13/class13-materials/bachelor-code/2/js/main.js
@@ -1,25 +1,31 @@
+//Write a program that shows the next bachelorettes img when their name is clicked on
+
+//create const that selects each bachelorettes img
 const andi = document.querySelector('#andi')
 const claire = document.querySelector('#claire')
-const sharleen = document.querySelector('#sharleen')
+const sharleen = document.querySelector('sharleen')
 
+//Create Event Listeners on each
 document.querySelector('#andiNext').addEventListener('click', andiNext)
 document.querySelector('#claireNext').addEventListener('click', claireNext)
 document.querySelector('#sharleenNext').addEventListener('click', sharleenNext)
 
+//Create a function that runs when a bachelorettes name is clicked
+
 function andiNext(){
+	andi.classList.toggle('hidden')
 	claire.classList.add('hidden')
 	sharleen.classList.add('hidden')
-	andi.classList.toggle("hidden")
 }
 
 function claireNext(){
+	claire.classList.toggle('hidden')
 	andi.classList.add('hidden')
 	sharleen.classList.add('hidden')
-	claire.classList.toggle('hidden')
 }
 
-function sharleenNext(){
-	claire.classList.add('hidden')
-	andi.classList.add('hidden')
+function sharleenNext() {
 	sharleen.classList.toggle('hidden')
+	andi.classList.add('hidden')
+	sharleen.classList.add('hidden')
 }

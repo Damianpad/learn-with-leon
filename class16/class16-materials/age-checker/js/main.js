f4d6a3855f0f2bc9c5d8b3a905f6f724144a4198
Damian Padilla <damipad4@gmail.com>
2022-10-25 10:40:54 -0500

    completed age-checker ex

--- class16/class16-materials/age-checker/js/main.js
+++ class16/class16-materials/age-checker/js/main.js
@@ -1,10 +1,37 @@
 //Create a conditonal that checks their age
+
 //If under 16, tell them they can not drive
 //If under 18, tell them they can't hate from outside the club, because they can't even get in
 //If under 21, tell them they can not drink
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
+
+let result;
+
+document.querySelector('h1').addEventListener('click', checkAge)
+
+
+
+function checkAge() {
+    let age = document.querySelector('#danceDanceRevolution').value
+
+    if (age < 16){
+        result = 'You can\'t drive'
+        document.querySelector('p').innerText = result;
+    } else if (age > 16 && age < 18) {
+        alert("You can't hate from outside the club, because you can't even get in.")
+    } else if (age > 18 && age < 21) {
+        alert("You can't drink.")
+    } else if (age > 21 && age < 25) {
+        alert("You can't rent cars affordably.")
+    } else if (age > 25 && age < 30) {
+        alert("You can't rent cars FANCY affordably.")
+    } else {
+        alert("There is nothing left to look forward to.")
+    } 
+}
+
 
 
 //--- Harder

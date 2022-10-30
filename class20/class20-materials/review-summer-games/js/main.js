7276b3cebe5d5d86f65237786109e5c25044fad3
Damian Padilla <damipad4@gmail.com>
2022-10-29 20:04:42 -0500

    Complete forEach exercise

--- class20/class20-materials/review-summer-games/js/main.js
+++ class20/class20-materials/review-summer-games/js/main.js
@@ -1,1 +1,29 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+// let myArray = [1,2,3,4]
+
+// function multiplyArr(arr) {
+//     return arr.reduce((a,b) => a*b)
+// }
+
+// console.log(multiplyArr(myArray));
+
+// function multiplyArr(arr) {
+//     let product = 1
+//     arr.forEach( num => product *= num )
+//     alert(product)
+// }
+
+// multiplyArr([10,2,3])
+
+
+let fruits = ['apples', 'oranges', 'grapes']
+
+fruits.forEach((x => {
+    console.log(x);
+}))
+
+let numbers = [1,2,3,4,5,6,7,8,9,10]
+
+numbers.forEach((x => {
+    console.log(x);
+}))

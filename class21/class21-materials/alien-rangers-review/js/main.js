a82faf9b3ac9823f15a65b1286fb878f0d8acfd8
Damian Padilla <damipad4@gmail.com>
2022-11-01 18:18:28 -0500

    SR on alien rangers

--- class21/class21-materials/alien-rangers-review/js/main.js
+++ class21/class21-materials/alien-rangers-review/js/main.js
@@ -6,11 +6,17 @@ tvShows.forEach((x => {
     console.log(x);
 }))
 //Create an array of numbers
-const sumNum = [1,2,3,4,5,6,7,8,9,10]
+const sumNum = [1,2,3,4,5,6,7,8,9,10,20,100]
 //Return a new array of numbers that includes every even number from the previous Arrays
 let newNum = []
-newNum.push(sumNum)
-console.log(newNum);
+for (i=0; i < sumNum.length; i++){
+    if (i % 2 == 0) {
+        newNum.push(i)
+    }
+}
+// newNum.push(sumNum)
+console.log(`New array is: ${newNum}`);
+console.log('hello');
 //Create a function that takes in an array of numbers
 
 //Alert the sum of the second lowest and the second highest number

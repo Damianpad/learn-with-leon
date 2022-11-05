f0004911e0b381651b05a8d00060791358460583
Damian Padilla <damipad4@gmail.com>
2022-11-05 16:28:34 -0500

    added func that runs multiple times

--- class22/class22-materials/turbo-review/js/main.js
+++ class22/class22-materials/turbo-review/js/main.js
@@ -45,5 +45,7 @@ choose('Rock')
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
 function results(arr){
-
+    arr.forEach(choice => choose(choice));
 }
+
+results(['Rock', 'Paper', 'Scissors'])

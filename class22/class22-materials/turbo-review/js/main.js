b02603acf193e4d8e32e307b62d09dace119ba62
Damian Padilla <damipad4@gmail.com>
2022-11-04 23:39:34 -0500

    Added function for RPS

--- class22/class22-materials/turbo-review/js/main.js
+++ class22/class22-materials/turbo-review/js/main.js
@@ -27,9 +27,18 @@ console.log(play());
 // *Conditionals*
 //Create a function that takes in a choice (rock, paper, or scissors) and determines if they won a game of rock paper scissors against a bot using the above function
 
-function choose(choice){
-
+function choose(playerChoice){
+    let botChoice = play()
+    if ((playerChoice == 'Rock' && botChoice == 'Scissors') || 
+    (playerChoice == 'Paper' && botChoice == 'Rock') ||
+    (playerChoice == 'Scissors' && botChoice == 'Paper')) {
+        console.log('YOU WIN');
+    } else {
+        'YOU LOSE'
+    }
 }
+
+choose('Rock')
 //*Loops*
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
 function results(arr){

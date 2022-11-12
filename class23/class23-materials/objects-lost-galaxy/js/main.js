f74230829ebf017dfb93f4c8f408ee29ae4be88c
Damian Padilla <damipad4@gmail.com>
2022-11-11 21:12:42 -0600

    completed dog object practice

--- class23/class23-materials/objects-lost-galaxy/js/main.js
+++ class23/class23-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,25 @@
 //Create a dog object that has four properties and three methods
+
+let dog = {}
+
+dog.color = 'brown'
+dog.breed = 'lab'
+dog.sex = 'male'
+dog.name = 'thor'
+
+dog.bark = function() {
+    console.log('WOOF WOOF');
+}
+
+dog.rollover = function() {
+    console.log('Rolling');
+}
+
+dog.sit = function() {
+    console.log('*sitting*');
+}
+
+console.log(dog.color);
+dog.bark()
+dog.rollover()
+dog.sit()

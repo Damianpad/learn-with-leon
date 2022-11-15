6d578e45b3e32661eb9471ac1b48e40edfb74a8b
Damian Padilla <damipad4@gmail.com>
2022-11-15 12:11:38 -0600

    practiced class constructor syntax

--- class26/class26-materials/objects-lost-galaxy/js/main.js
+++ class26/class26-materials/objects-lost-galaxy/js/main.js
@@ -55,3 +55,73 @@ let sophiaChoice = new MakePizza('Pep', 'Regular', 'Small')
 console.log(sophiaChoice.toppings);
 console.log(sophiaChoice.crust);
 console.log(sophiaChoice.size);
+
+// Make a Car object with literal notation
+let car = {}
+
+car.make = 'Honda'
+car.model = 'Accord'
+car.year = 1999
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+
+// Make a car with a constructor
+// function MakeCar(make, model, year) {
+//     this.make = make
+//     this.model = model
+//     this.year = year
+// }
+
+// let damianCar = new MakeCar('Mazda', '3', '2009')
+// console.log(damianCar.make);
+// console.log(damianCar.model);
+// console.log(damianCar.year);
+
+// Make a car constructor using es6 updates (class)
+class MakeCar{
+    constructor(make,model,year){
+        this.make = make
+        this.model = model
+        this.year = year
+    }
+    honk(){
+        console.log('BEEP BEEP');
+    }
+    lock(){
+        alert(`Locked doors!`)
+    }
+
+}
+
+let anaCar = new MakeCar('Honda', 'Civic', 2012)
+console.log(anaCar.make);
+console.log(anaCar.model);
+console.log(anaCar.year);
+anaCar.honk()
+anaCar.lock()
+
+
+// Make a Pet with the class constructor syntax
+class MakePet{
+    constructor(breed, color, age){
+        this.breed = breed
+        this.color = color
+        this.age = age
+    }
+
+    bark(){
+        console.log('WOOF WOOF');
+    }
+    sit(){
+        console.log('*SITS*');
+    }
+}
+
+let cooper = new MakePet('Aussie', 'white and black', '6 Months')
+console.log(cooper.breed);
+console.log(cooper.color);
+console.log(cooper.age);
+cooper.bark()
+cooper.sit()

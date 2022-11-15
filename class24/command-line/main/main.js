de04a3cb53e47bdcc4a1dafa9f6e6d79dc96d5ba
Damian Padilla <damipad4@gmail.com>
2022-11-14 23:21:21 -0600

    object practice

--- class24/command-line/main/main.js
+++ class24/command-line/main/main.js
@@ -3,3 +3,14 @@ let arr = [1,2,3,4,5]
 let sum = arr.reduce((acc, c) => acc + c ,0)
 
 console.log(sum);
+
+// Write a function that checks if a person is a member.
+// Use the ternary operator to see if is a member. 
+// If member: FREE
+// ELse: $10 FEE
+
+function getFee(isMember) {
+    return (isMember ? 'FREE' : '$10.00')
+}
+
+console.log(getFee(false));
---
+++ class26/class26-materials/objects-lost-galaxy/js/main.js
@@ -0,0 +1,57 @@
+//Create a pizza object that has four properties and three methods
+
+let pizza = {} 
+
+pizza.toppings = ['Pineapple', 'Canadian Bacon']
+pizza.crust = 'Stuffed'
+pizza.size = 'Large'
+
+pizza.delivered = function(){
+    console.log('Time: Until delivered');
+}
+
+pizza.burnMouth = function(){
+    console.log('Owie');
+}
+
+pizza.frisbee = function() {
+    console.log('YEET');
+}
+
+// Make a constructor that makes a pizza
+function MakePizza(pizzaToppings, pizzaCrust, pizzaSize){
+    this.toppings = pizzaToppings
+    this.crust = pizzaCrust
+    this.size = pizzaSize
+    this.delivered = function(){
+        return 'Calculating...'
+    }
+    this.burnMouth = function() {
+        console.log('Owiee');
+    }
+    this.frisbee = function() {
+        console.log('YEEET');
+    }
+} 
+
+let damianChoice = new MakePizza('Pineapple', 'Stuffed', 'Large')
+
+console.log(damianChoice.toppings);
+console.log(damianChoice.crust);
+console.log(damianChoice.size);
+console.log(damianChoice.delivered);
+
+let anaChoice = new MakePizza('Meat Lovers', 'Garlic', 'Medium')
+console.log(anaChoice.toppings);
+console.log(anaChoice.crust);
+console.log(anaChoice.size);
+
+let lizzyChoice = new MakePizza('Cheese', 'Regular', 'Medium')
+console.log(lizzyChoice.toppings);
+console.log(lizzyChoice.crust);
+console.log(lizzyChoice.size);
+
+let sophiaChoice = new MakePizza('Pep', 'Regular', 'Small')
+console.log(sophiaChoice.toppings);
+console.log(sophiaChoice.crust);
+console.log(sophiaChoice.size);

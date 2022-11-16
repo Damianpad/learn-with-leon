9bf3c11f3f6a0b6f3ac133c9980f01197fc2a61f
Damian Padilla <damipad4@gmail.com>
2022-11-16 14:29:27 -0600

    completed api practice

--- class26/class26-materials/objects-dog-photos/js/main.js
+++ class26/class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,10 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())// parse object as json
+    .then(data => {
+        console.log(data);
+        document.querySelector('.dogImg').src = data.message
+    })
+    .catch(err => {
+        console.log(`error ${err}`);
+    })

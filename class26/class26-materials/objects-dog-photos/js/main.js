38f57c1bb14cf26a75bdbec37760a9c301b766e8
Damian Padilla <damipad4@gmail.com>
2022-11-16 21:58:52 -0600

    added doggo generator

--- class26/class26-materials/objects-dog-photos/js/main.js
+++ class26/class26-materials/objects-dog-photos/js/main.js
@@ -1,5 +1,8 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
-fetch('https://dog.ceo/api/breeds/image/random')
+document.querySelector('.getDog').addEventListener('click', getDoggo)
+
+function getDoggo(){
+    fetch('https://dog.ceo/api/breeds/image/random')
     .then(res => res.json())// parse object as json
     .then(data => {
         console.log(data);
@@ -8,3 +11,4 @@ fetch('https://dog.ceo/api/breeds/image/random')
     .catch(err => {
         console.log(`error ${err}`);
     })
+}
--- class26/class26-materials/objects-shots/js/main.js
+++ class26/class26-materials/objects-shots/js/main.js
@@ -1,1 +1,9 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+fetch('www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka')
+    .then(res => res.json())// parse object as json
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(`error ${err}`);
+    })

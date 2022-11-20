457aeb583f01c1821bfcc6d6015ea51d9e06c770
Damian Padilla <damipad4@gmail.com>
2022-11-20 11:46:57 -0600

    created local storage counter

--- class27/class27-materials/local-storage-botScore-button/js/main.js
+++ class27/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,25 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
+if (!localStorage.getItem('botScore')){
+    localStorage.setItem('botScore', 0)
+}
+
+const startingScore = localStorage.getItem('botScore')
+
+document.querySelector('h2').innerText = startingScore
+document.querySelector('.add').addEventListener('click', addScore)
+document.querySelector('.clear').addEventListener('click', clearScore)
+
+function addScore(){
+    let botScoreVal = Number(localStorage.getItem('botScore'))
+    botScoreVal += 1
+    localStorage.setItem('botScore', botScoreVal)
+    document.querySelector('h2').innerText = botScoreVal
+}
+
+function clearScore(){
+    console.log('clear');
+    localStorage.clear()
+    document.querySelector('h2').innerText = 0
+}
 

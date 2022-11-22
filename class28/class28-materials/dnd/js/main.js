eeda40690ad98fcebc55326669f4f51410de3390
Damian Padilla <damipad4@gmail.com>
2022-11-22 16:19:36 -0600

    up save

--- class28/class28-materials/dnd/js/main.js
+++ class28/class28-materials/dnd/js/main.js
@@ -8,7 +8,16 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        document.querySelector('li').remove
+        subclassesArr = data.subclasses
+        subclassesArr.forEach(element => {
+            console.log(element.name);
+
+            let el = document.createElement('li')
+            el.innerHTML = element.name
+            document.querySelector('ul').appendChild(el)
+
+        });
       })
       .catch(err => {
           console.log(`error ${err}`)

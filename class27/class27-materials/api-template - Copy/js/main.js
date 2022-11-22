a0efbdfc9b2e31e6c0376876a7bde330907f2cfe
Damian Padilla <damipad4@gmail.com>
2022-11-21 19:45:52 -0600

    Added war logic and reset

--- class27/class27-materials/api-template - Copy/js/main.js
+++ class27/class27-materials/api-template - Copy/js/main.js
@@ -1,6 +1,8 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', drawTwo)
 let deckId = ''
+let player1Score = 0
+let player2Score = 0
 
   // Get Deck
   fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
@@ -16,6 +18,8 @@ let deckId = ''
   
 
 function drawTwo(){
+  resetWarCards()
+
   fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
@@ -23,8 +27,26 @@ function drawTwo(){
         console.log(data.cards[0]);
         let player1CardImage = data.cards[0].image
         let player2CardImage = data.cards[1].image
-        let player1Card = data.cards[0].value + " " + data.cards[0].suit
-        let player2Card = data.cards[1].value + " " + data.cards[1].suit
+        
+        let player1Value = convertToNum(data.cards[0].value)
+        let player2Value = convertToNum(data.cards[1].value)
+
+        if (player1Value > player2Value){
+          player1Score += 1
+          document.querySelector("#result").innerText = "Result: Player 1 Wins"
+          document.querySelector('#player1-score').innerText = player1Score
+
+        } else if (player2Value > player1Value){
+          player2Score += 1
+          document.querySelector("#result").innerText = "Result: Player 2 Wins"
+          document.querySelector('#player2-score').innerText = player2Score
+        } else {
+          document.querySelector("#result").innerText = "Result: WAR"
+          runWar()
+        }
+
+        let player1Card = player1Value + " " + data.cards[0].suit
+        let player2Card = player2Value + " " + data.cards[1].suit
         
         document.querySelector('#player1').src = player1CardImage
         document.querySelector('#player2').src = player2CardImage
@@ -36,6 +58,45 @@ function drawTwo(){
       });
 }
 
+function convertToNum(value){
+  if (value === 'ACE'){
+    return 14
+  } else if (value === 'KING'){
+    return 13
+  } else if (value === 'QUEEN'){
+    return 12
+  } else if (value === 'JACK'){
+    return 11
+  } else {
+    return value
+  }
+}
+
+function runWar(){
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    let player1WarImage = data.cards[0].image
+    document.querySelector('.player1-war-img').src = player1WarImage
+    document.querySelector('.war-result1').innerText = data.cards[0].value + " " + data.cards[0].suit
+
+    let player2WarImage = data.cards[1].image
+    document.querySelector('.player2-war-img').src = player2WarImage
+    document.querySelector('.war-result2').innerText = data.cards[1].value + " " + data.cards[1].suit
+  })
+  .catch(err => {
+    console.log(`error ${err}`)
+  })
+}
+
+function resetWarCards(){
+  document.querySelector('.player1-war-img').src = ""
+  document.querySelector('.war-result1').innerText = ""
+
+  document.querySelector('.player2-war-img').src = ""
+  document.querySelector('.war-result2').innerText = ""
+}
 
 
 

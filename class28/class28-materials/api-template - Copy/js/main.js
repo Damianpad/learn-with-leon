928698486488c8085c3aae4aa85502677e589469
Damian Padilla <damipad4@gmail.com>
2022-11-24 01:30:32 -0600

    init open library search

--- class28/class28-materials/api-template - Copy/js/main.js
+++ class28/class28-materials/api-template - Copy/js/main.js
@@ -4,21 +4,32 @@ document.querySelector('button').addEventListener('click', getFetch)
 function getFetch(){
   const choice = document.querySelector('input').value
   console.log(choice)
-  const url = `https://openlibrary.org/works/OL45883W/Fantastic_Mr._FOX`
+  const url = `http://openlibrary.org/search.json?q=${choice}`
+  // const url = `https://openlibrary.org/isbn/${choice}.json`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        // if(data.media_type === "image"){
-        //   document.querySelector('img').src = data.hdurl
-        // }else if(data.media_type === 'video'){
-        //   document.querySelector('iframe').src = data.url
-        // }else{
-        //   alert('Media Not Supported - Contact NASA IMMEDIATLY')
-        // }
-       
-        // document.querySelector('h3').innerText = data.explanation
+        let bookCover = `https://covers.openlibrary.org/b/id/${data.docs.covers_i}.jpg`
+        let bookTitle = data.docs.title
+        document.querySelector('.book-cover').src = bookCover
+        document.querySelector('.book-title').innerText = bookTitle
+        // console.log(data.docs[0])
+
+
+        // document.querySelector('li').remove
+        titleArr = data.docs
+        titleArr.forEach(element => {
+            console.log(element.title);
+
+
+            let el = document.createElement('li')
+            el.innerHTML = element.title
+            document.querySelector('ul').appendChild(el)
+
+        });
+
       })
       .catch(err => {
           console.log(`error ${err}`)

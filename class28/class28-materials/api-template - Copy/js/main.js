2ad51f1e87ff1906e0eb0f2099ca5869c8f4aebf
Damian Padilla <damipad4@gmail.com>
2022-11-24 23:35:29 -0600

    init search logic fot book

--- class28/class28-materials/api-template - Copy/js/main.js
+++ class28/class28-materials/api-template - Copy/js/main.js
@@ -11,10 +11,10 @@ function getFetch(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        let bookCover = `https://covers.openlibrary.org/b/id/${data.docs.covers_i}.jpg`
+        
         let bookTitle = data.docs.title
-        document.querySelector('.book-cover').src = bookCover
-        document.querySelector('.book-title').innerText = bookTitle
+        // document.querySelector('.book-cover').src = bookCover
+        // document.querySelector('.book-title').innerText = bookTitle
         // console.log(data.docs[0])
 
 
@@ -22,11 +22,21 @@ function getFetch(){
         titleArr = data.docs
         titleArr.forEach(element => {
             console.log(element.title);
-
-
+            
+            // let bookCover = element.docs.covers_i
+            
+            // document.querySelector('.book-cover').src = bookCoverLink
+            
             let el = document.createElement('li')
             el.innerHTML = element.title
-            document.querySelector('ul').appendChild(el)
+            document.querySelector('.book-results').appendChild(el)
+
+            console.log(element.cover_i);
+            let bookImg = document.createElement('img')
+            bookImg.src = `https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`
+            document.querySelector('.book-results').appendChild(bookImg)
+
+
 
         });
 

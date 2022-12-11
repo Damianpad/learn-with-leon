5ab8cf6be2e39b16cae0070bcec019eb35e2f3ec
Damian Padilla <damipad4@gmail.com>
2022-12-11 00:39:07 -0600

    complete age checker

--- super-review/super-review/age-checker/js/main.js
+++ super-review/super-review/age-checker/js/main.js
@@ -5,9 +5,45 @@
 //If under 25, tell them they can not rent cars affordably
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
+// let age = 28
 
+// if (age < 16){
+//     console.log('You cannot drive');
+// } else if (age < 18) {
+//     console.log('You can\'t hate from outside the club, because you can\'t even get in');
+// } else if (age < 21) {
+//     console.log('You can\'t drink');
+// } else if (age < 25){
+//     console.log('You can\'t rent cars affordably');
+// } else if (age <= 30) {
+//     console.log('You can\'t rent fancy cars affordably');
+// } else if (age > 30) {
+//     console.log('There is nothing left to look forward to');
+// }
 
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+const check = document.querySelector('h1')
+
+check.addEventListener('click', checkAge)
+
+function checkAge() {
+    const age = Number(document.querySelector('#danceDanceRevolution').value)
+    console.log(age);
+
+    if (age < 16){
+        console.log('You cannot drive');
+    } else if (age < 18) {
+        console.log('You can\'t hate from outside the club, because you can\'t even get in');
+    } else if (age < 21) {
+        console.log('You can\'t drink');
+    } else if (age < 25){
+        console.log('You can\'t rent cars affordably');
+    } else if (age <= 30) {
+        console.log('You can\'t rent fancy cars affordably');
+    } else if (age > 30) {
+        console.log('There is nothing left to look forward to');
+    }
+}

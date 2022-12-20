9872474aedd35256fdb44d7a2b69a05547b0cef6
Damian Padilla <damipad4@gmail.com>
2022-12-19 20:06:20 -0600

    added async await template

--- class36/class36-materials/api-template/js/main.js
+++ class36/class36-materials/api-template/js/main.js
@@ -1,1 +1,16 @@
 //Make an api request using async await
+document.querySelector('button').addEventListener('click', getCuteDogPhoto)
+
+async function getCuteDogPhoto(){
+        const res = await fetch('https://dog.ceo/api/breeds/image/random')
+        const data = await res.json()
+    try {
+        console.log(data)
+        document.querySelector('img').src = data.message
+    } 
+    catch {
+        console.log('error');
+    }
+}
+
+getCuteDogPhoto()

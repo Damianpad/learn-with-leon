395476665cddd3abc3c765e74f2c0288e4f77826
Damian Padilla <damipad4@gmail.com>
2023-01-01 11:59:01 -0600

    created rapper names api

--- class38/class38-materials/rap-names-api/server.js
+++ class38/class38-materials/rap-names-api/server.js
@@ -1,9 +1,38 @@
+const { request } = require('express')
 const express = require('express')
 const app = express()
 const PORT = 8000
 
+const rappers = {
+    '21 Savage': {
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England'
+    },
+    'Chance The Rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'Dylan': {
+        'age': 29,
+        'birthName': 'Dylan',
+        'birthLocation': 'Dylan'
+    }
+}
+
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName', (req, res)=>{
+    const rappersName = req.params.rapperName
+    if(rappers[rappersName]){
+        res.json(rappers[rappersName])
+    } else {
+        res.json(rappers['Dylan'])
+    }
+    //res.json(rappers)
 })
 
 app.listen(PORT, () => {

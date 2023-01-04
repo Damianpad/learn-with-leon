baafa34f2d71b49d3c6f4a442875f418b3babe10
Damian Padilla <damipad4@gmail.com>
2023-01-03 22:44:36 -0600

    added fetch to marvel api

--- class38/class38-materials/client-side-code/js/main.js
+++ class38/class38-materials/client-side-code/js/main.js
@@ -1,13 +1,13 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-    const rapperName = document.querySelector('input').value
+    const heroName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://simple-rapper-api.herokuapp.com/api/${rapperName}`)
+        const response = await fetch(`https://easy-pink-clam-cap.cyclic.app/api/${heroName}`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data.birthName
+        document.querySelector('h2').innerText = data.alterEgo
     }catch(error){
         console.log(error)
     }

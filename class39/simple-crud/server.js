f005138d61ecdce5a369ce165a9d4c783d303c83
Damian Padilla <damipad4@gmail.com>
2023-01-08 18:28:05 -0600

    added to mongodb

--- class39/simple-crud/server.js
+++ class39/simple-crud/server.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 const PORT = 3000
+
+const connectionString =     `mongodb+srv://yoda:Lilelizabeth1.@cluster0.ejqzl9h.mongodb.net/?retryWrites=true&w=majority`
+
+MongoClient.connect(connectionString, { useUnifiedTopology : true })
+    .then (client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    // app.use()
+    // app.get()
+    // app.post()
+    // app.listen()
+})
+    .catch(console.error)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -13,5 +30,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body);
+    quotesCollection.insertOne(req,body)
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => console.error(error))
 })

45310ab45d030abd3978996319aee2c86183d1dc
Damian Padilla <damipad4@gmail.com>
2023-01-11 21:47:13 -0600

    updated server

--- class39/simple-crud/server.js
+++ class39/simple-crud/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { response } = require('express')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 const PORT = 3000
 
 const connectionString = `mongodb+srv://yoda:Lilelizabeth1.@cluster0.ejqzl9h.mongodb.net/?retryWrites=true&w=majority`
+
 
 MongoClient.connect(connectionString, { useUnifiedTopology : true })
     .then (client => {
@@ -20,12 +22,21 @@ MongoClient.connect(connectionString, { useUnifiedTopology : true })
     app.use(bodyParser.urlencoded({ extended: true }))
 
     app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html')
+        db.collection('quotes').find().toArray()
+        .then(data => {
+            console.log(data);
+            res.sendFile(__dirname + '/index.html')
+            // res.render('index.ejs', { info: data})
+        })
+       
+        .catch(error => console.error(error))
+        
     })
 
     app.post('/quotes', (req, res) => {
         quotesCollection.insertOne(req.body)
             .then(result => {
+                
                 console.log(result);
             })
             .catch(error => console.error(error))
